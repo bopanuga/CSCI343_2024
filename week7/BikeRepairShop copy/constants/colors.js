cb9e5964cfd506c9ec9a9cc605a23d8a433efae6
bopanuga <bopanuga@coastal.edu>
2024-10-09 23:57:19 -0400

    Delete week7/BikeRepairShop copy directory

--- week7/BikeRepairShop copy/constants/colors.js
+++
@@ -1,9 +0,0 @@
-// RESOURCE FILE FOR STYLING FOR THE COLOR SCHEME OF THE BIKE REPAIR APP
-const Colors = {
-    accent500: "#d8cab2",
-    primary300: "##c76335",
-    primary500: "##6a7b8c",
-    primary800: "#071013",
-};
-
-export default Colors;
